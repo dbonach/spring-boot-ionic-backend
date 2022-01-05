7980baf164cd1c949b10d05ab2a86566d5f85f8e
dbonach <deivitlopes@gmail.com>
2022-01-05 10:24:42 -0300

    Enviando imagens para aws via endpoint

--- src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Cliente;
@@ -91,5 +92,12 @@ public class ClienteResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value="/picture")
+	public ResponseEntity<URI> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		
+		URI uri = service.uploadProfilePicture(file);		
+		return ResponseEntity.created(uri).build();
 	}
 }
