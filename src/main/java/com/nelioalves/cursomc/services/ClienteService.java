f963cb0193237a1821071332b6701fe68fdaf0b5
dbonach <deivitlopes@gmail.com>
2022-01-05 13:28:00 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -118,6 +118,22 @@ public class ClienteService {
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
 		
-		return s3service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		URI uri = s3service.uploadFile(multipartFile);
+		
+		Optional<Cliente> obj = repo.findById(user.getId());
+		
+		Cliente cli = obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName()));
+	
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+		
+		return uri;
 	}
 }
